35b28139a51c0266464689811c8b360784b9cadc
nickpesce <nickpesce22@gmail.com>
2015-09-22 20:38:28 -0400

    initial commit

---
+++ app/src/main/java/com/github/nickpesce/Component/Entity.java
@@ -0,0 +1,162 @@
+package com.github.nickpesce.component;
+
+import android.graphics.Rect;
+
+import com.github.nickpesce.downup.GameActivity;
+import com.github.nickpesce.drawing.Sprite;
+
+import java.util.Iterator;
+
+/**
+ * The up/down items in the game. The things that are moving. anchors, balloons, etc.
+ */
+public class Entity
+{
+    //All in terms of 3840x2160
+    private GameActivity game;
+    private int width;
+    private int height;
+    private double x;
+    private double y;
+    private double vX;
+    private double vY;
+    private double aX;
+    private double aY;
+    private Sprite sprite;
+    private int n;
+
+    public Entity(GameActivity game, double x, double y, int width, int height, int n)
+    {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.aY = Math.random()-.5;
+        this.n = n;
+        if(aY > 0)
+            sprite = game.getRandomDownSprite(width, height);
+        else
+            sprite = game.getRandomUpSprite(width, height);
+    }
+
+    public void update()
+    {
+        x += vX;
+        y += vY;
+        vX += aX;
+        vY += aY;
+        if(y <= 0)
+            hitTop();
+        else if(y >= 3840 - height)
+            hitBottom();
+        sprite.update(getDrawRect());
+    }
+
+    private void hitBottom()
+    {
+        game.gameOver();
+    }
+
+    private void hitTop()
+    {
+        game.gameOver();
+    }
+    public void reverse()
+    {
+        vY = 0;
+        if(aY<0)
+        {
+            this.aY = Math.random()/2;
+            sprite = game.getRandomDownSprite(width, height);
+        }
+        else
+        {
+            this.aY = -Math.random()/2;
+            sprite = game.getRandomUpSprite(width, height);
+        }
+        //Check all objectives. If in same row, remove and apply effect if in zone.
+        Iterator<Objective> it = game.getObjectives().iterator();
+        while(it.hasNext())
+        {
+            Objective o = it.next();
+            if(o.getColumnNumber() == n)
+            {
+                if(o.isTouching(getRect()))
+                    o.hit(this);
+                it.remove();
+            }
+        }
+        if(Math.random() < .2)
+        {
+            game.getObjectives().add(Objective.getNewRandomObjective(n, aY < 0));
+        }
+    }
+
+
+    public Rect getRect()
+    {
+        return new Rect((int)x, (int)y, (int)(x +  width), (int)(y+height));
+    }
+
+    public Rect getDrawRect()
+    {
+        return game.getGameView().applyTransformation(getRect(), vX, vY);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getvX() {
+        return vX;
+    }
+
+    public void setvX(double vX) {
+        this.vX = vX;
+    }
+
+    public double getvY() {
+        return vY;
+    }
+
+    public void setvY(double vY) {
+        this.vY = vY;
+    }
+
+    public double getaX() {
+        return aX;
+    }
+
+    public void setaX(double aX) {
+        this.aX = aX;
+    }
+
+    public double getaY() {
+        return aY;
+    }
+
+    public void setaY(double aY) {
+        this.aY = aY;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+}
---
+++ app/src/main/java/com/github/nickpesce/Component/Objective.java
@@ -0,0 +1,102 @@
+package com.github.nickpesce.component;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import com.github.nickpesce.downup.GameActivity;
+import com.github.nickpesce.drawing.GameView;
+
+/**
+ * A section of a column that will give a reward if the item is "reversed" while on top of it. Removed when the item in that column is reversed.
+ */
+public class Objective
+{
+    public enum Type
+    {slow, points, gem, extra}
+
+    private int n;
+    private int y;
+    private int width;
+    private Type type;
+    private int color;
+    private Rect rect;
+
+    public Objective(int n, Type type, int y, int width)
+    {
+        this.n = n;
+        this.type = type;
+        switch(type)
+        {
+            case slow:
+                this.color = Color.CYAN;
+                break;
+            case points:
+                this.color = Color.RED;
+                break;
+            case gem:
+                this.color = Color.YELLOW;
+                break;
+            case extra:
+                this.color = Color.BLUE;
+                break;
+            default:
+                this.color = Color.BLACK;
+                break;
+
+        }
+        this.y = y;
+        this.width = width;
+        rect = new Rect((int)((GameActivity.WIDTH/4.0) * n), y, (int)((GameActivity.WIDTH/4.0) * (n+1)), y+width);
+    }
+
+    public void hit(Entity e)
+    {
+        e.setaY(0);
+        e.setvY(0);
+    }
+    public boolean includesPoint(int locY)
+    {
+        return locY < y && locY > y+width;
+    }
+
+    public boolean isTouching(Rect other)
+    {
+        return Rect.intersects(other, rect);
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public int getColumnNumber()
+    {
+        return n;
+    }
+
+    public void draw(Canvas canvas, GameView gameView)
+    {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(gameView.applyTransformation(rect),  paint);
+    }
+
+    public static Objective getNewRandomObjective(int n, boolean top)
+    {
+        int width = (int)(Math.random() * (GameActivity.HEIGHT/10.0) + 20);//At least 20. Max height/10 + 20
+        Type type = Type.values()[(int)(Math.random()*Type.values().length)];
+        int y;
+        if(top)
+        {
+            y = (int)(Math.random() * (GameActivity.HEIGHT/4.0));//y value in top fourth of screen
+        }
+        else
+        {
+            y = GameActivity.HEIGHT - (int)(Math.random() * (GameActivity.HEIGHT/4.0)) - width;//y value in bottom fourth of screen
+        }
+
+        return new Objective(n, type, y, width);
+    }
+}
