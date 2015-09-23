d25ca6815ac5cb70013a12d60b250c08df69e649
nickpesce <nickpesce22@gmail.com>
2015-09-22 23:06:42 -0400

    Put the device in sticky immersive mode if applicable.

--- app/src/main/java/com/github/nickpesce/downup/GameActivity.java
+++ app/src/main/java/com/github/nickpesce/downup/GameActivity.java
@@ -2,9 +2,11 @@ package com.github.nickpesce.downup;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.github.nickpesce.component.Entity;
 import com.github.nickpesce.component.Objective;
@@ -38,6 +40,15 @@ public class GameActivity extends Activity {
         loop = new GameLoop(this);
         paused = false;
         startTime = System.currentTimeMillis();
+        if(Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
     }
 
     public void onTouch(int x, int y)
